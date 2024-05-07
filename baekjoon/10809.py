641f0787e2e0c29dd51790de4826ef9a9e7f5ca4
yujunggg <a90134627@gmail.com>
2024-05-07 17:12:54 +0900

    1152.py
    백준

---
+++ baekjoon/10809.py
@@ -0,0 +1,8 @@
+s = list(input())
+eng = 'abcdefghijklmnopqrstuvwxyz'
+
+for i in eng:
+    if i in s:
+        print(s.index(i), end=' ')
+    else:
+        print(-1, end=' ')
--- baekjoon/2675.py
+++ baekjoon/2675.py
@@ -1,17 +1,8 @@
-# n = int(input())
-# for i in range(n):
-#     m,s = input().split()
-#     s = list(s)
-#     m = int(m)
-#     for j in range(m):
-#         print(s[j]*m, end='')
-#     print()
-
 n = int(input())
 for i in range(n):
     m, s = input().split()
     s = list(s)
     m = int(m)
-    for j in range(min(m, len(s))):
+    for j in range(len(s)):
         print(s[j] * m, end='')
     print()
