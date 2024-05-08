d396b3445e88817ba7e8e74e9441ff01a3359795
yujunggg <a90134627@gmail.com>
2024-05-08 20:46:23 +0900

    1316.py
    백준

--- baekjoon/1157.py
+++ baekjoon/1157.py
@@ -1,22 +1,35 @@
-s = input()
-s = s.upper()
+# s = input()
+# s = s.upper()
 
-s = list(s)
+# s = list(s)
 
-temp = 0
-case = ''
-res = 0
+# temp = 0
+# case = ''
+# res = 0
 
-for i in s:
-    temp2 = s.count(i)
-    if temp < temp2:
-        temp = temp2
-        case = i
-        res = 0
-    elif temp == temp2 and case != i:
-        res += 1
+# for i in s:
+#     temp2 = s.count(i)
+#     if temp < temp2:
+#         temp = temp2
+#         case = i
+#         res = 0
+#     elif temp == temp2 and case != i:
+#         res += 1
 
-if not res:
-    print(case)
+# if not res:
+#     print(case)
+# else:
+#     print('?')
+
+word = input().upper()
+word_list = list(set(word))
+lst = []
+
+for i in word_list:
+    count = word.count(i)
+    lst.append(count)
+
+if lst.count(max(lst))>= 2:
+    print("?")
 else:
-    print('?')
+    print(word_list[lst.index(max(lst))])
---
+++ baekjoon/1316.py
@@ -0,0 +1,8 @@
+n = int(input())
+li = []
+c = 0
+for i in range(n):
+    s = input()
+    li.append(s)
+for i in li:
+    
