8f55f13d6e8c3199ce8760279ee4845ae67be0c2
yujunggg <a90134627@gmail.com>
2024-06-04 20:32:24 +0900

    10718.py
    baekjoon

--- codeup/1261.py
+++ codeup/1261.py
@@ -1,5 +1,11 @@
-n = list(map(int,input().split()))
-for i in range(len(n)):
-    if n[i]%5==0:
-        print(n[i])
+n = map(int, input().split())
+n = list(n)
+b=0
+for i in n:
+    if i%5==0:
+        print(i)
         break
+    else:
+        b+=1
+if b==len(n):
+    print(0)
