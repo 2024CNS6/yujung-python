59acc0a3fac9b2b0fe9caa4bab5571360cc18933
yujunggg <a90134627@gmail.com>
2024-06-10 19:41:23 +0900

    2752.py
    baekjoon

--- baekjoon/2742.py
+++ baekjoon/2742.py
@@ -1,3 +1,13 @@
-n = int(input())
-for i in range(n,0,-1):
-    print(i)
+a,b,c = map(int,input().split())
+while True:
+    if a<b and a<c:
+        print(a, end=' ')
+        a = 1000000
+    elif b<a and b<c:
+        print(b, end=' ')
+        b = 1000000
+    elif c<b and c<a:
+        print(c, end=' ')
+        c = 1000000
+    if a == 1000000 and b == 1000000 and c == 1000000:
+        break
