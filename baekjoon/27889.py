5e2392b0c7888cdaa762bc1fd153e1ca5533a67c
yujunggg <a90134627@gmail.com>
2024-06-14 20:17:43 +0900

    27889.py
    baekjoon

---
+++ baekjoon/27889.py
@@ -0,0 +1,13 @@
+ch=input()
+
+if ch=="NLCS":
+    print("North London Collegiate School")
+
+elif ch=="BHA":
+    print("Branksome Hall Asia")
+
+elif ch=="KIS":
+    print("Korea International School")
+
+elif ch=="SJA":
+    print("St. Johnsbury Academy")
