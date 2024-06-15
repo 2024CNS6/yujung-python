7378ca928d482cd0d457790076a15fc3e73ac6d3
yujunggg <a90134627@gmail.com>
2024-06-15 20:36:36 +0900

    30007.py
    baekjoon

--- baekjoon/27433.py
+++ baekjoon/27433.py
@@ -1,9 +1,9 @@
-n = int(input())
+N = int(input())
 
-if n == 0:
+if N == 0 :
     print(1)
-else:
+else :
     result = 1
-    for i in range(2, n+1):
-        result += i
+    for i in range(2, N+1) :
+        result *= i
     print(result)
